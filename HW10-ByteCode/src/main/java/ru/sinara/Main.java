4b53d88820a4641359d5d1680bf248380098179a
papaAndrew <a.hermit71@gmail.com>
2025-08-19 17:50:40 +0500

    HW10-ByteCode fix Ioc

--- HW10-ByteCode/src/main/java/ru/sinara/Main.java
+++ HW10-ByteCode/src/main/java/ru/sinara/Main.java
@@ -3,7 +3,7 @@ package ru.sinara;
 public class Main {
     public static void main(String[] args) {
 
-        TestLogging testLogging = Ioc.patchTestLogging(new TestLoggingImpl());
+        TestLogging testLogging = Ioc.patch(new TestLoggingImpl());
         testLogging.calculate(6);
     }
 }
