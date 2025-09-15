01e4c2a0ba1a1908d34a98eb23c916cd7e103434
papaAndrew <a.hermit71@gmail.com>
2025-09-15 21:59:20 +0500

    HW16-Serialization improved 1

--- HW18-JDBC/build.gradle.kts
+++ HW18-JDBC/build.gradle.kts
@@ -1,6 +1,4 @@
 dependencies {
-    implementation(project(":L18-jdbc:demo"))
-
     implementation("ch.qos.logback:logback-classic")
     implementation("org.flywaydb:flyway-core")
     implementation("org.postgresql:postgresql")
