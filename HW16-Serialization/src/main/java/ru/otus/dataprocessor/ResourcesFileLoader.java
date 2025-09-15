442fe2cdba885a75073dfccbf492efb142382963
papaAndrew <a.hermit71@gmail.com>
2025-09-15 21:55:00 +0500

    HW16-Serialization improved 1

--- HW16-Serialization/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
+++ HW16-Serialization/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
@@ -4,7 +4,9 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.List;
 import org.slf4j.Logger;
@@ -27,13 +29,22 @@ public class ResourcesFileLoader implements Loader {
     public List<Measurement> load() {
         // читает файл, парсит и возвращает результат
         try {
-            return mapper.readValue(getUrl(), new TypeReference<>() {});
+            return mapper.readValue(getFile(), new TypeReference<>() {});
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new FileProcessException(e);
         }
     }
 
-    private URL getUrl() {
-        return this.getClass().getClassLoader().getResource(fileName);
+    private File getFile() {
+        ClassLoader classLoader = getClass().getClassLoader();
+        URL resource = classLoader.getResource(fileName);
+        if (resource == null) {
+            throw new FileProcessException("file not found! " + fileName);
+        }
+        try {
+            return new File(resource.toURI());
+        } catch (URISyntaxException e) {
+            throw new FileProcessException(e);
+        }
     }
 }
