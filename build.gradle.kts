47c020f8ccf180d71201830fdea8c233b221fa49
papaAndrew <a.hermit71@gmail.com>
2025-10-20 22:09:40 +0500

    HW28-springDataJdbc save

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 import io.spring.gradle.dependencymanagement.dsl.DependencyManagementExtension
-import org.gradle.plugins.ide.idea.model.IdeaLanguageLevel
+import name.remal.gradle_plugins.sonarlint.SonarLintExtension
 import org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES
+import org.gradle.plugins.ide.idea.model.IdeaLanguageLevel
 
 plugins {
     idea
@@ -23,7 +24,7 @@ idea {
 
 
 allprojects {
-    group = "ru.sinara"
+    group = "ru.otus"
 
     repositories {
         mavenLocal()
@@ -33,7 +34,23 @@ allprojects {
     val testcontainersBom: String by project
     val protobufBom: String by project
     val guava: String by project
+    val jmh: String by project
+    val asm: String by project
     val glassfishJson: String by project
+    val errorProneAnnotations: String by project
+    val j2objcAnnotations: String by project
+    val redisson: String by project
+
+    val jetty: String by project
+    val freemarker: String by project
+
+    val reflections: String by project
+
+    val sockjs: String by project
+    val stomp: String by project
+    val bootstrap: String by project
+    val springDocOpenapiUi: String by project
+    val jsr305: String by project
 
     apply(plugin = "io.spring.dependency-management")
     dependencyManagement {
@@ -44,7 +61,32 @@ allprojects {
                 mavenBom("com.google.protobuf:protobuf-bom:$protobufBom")
             }
             dependency("com.google.guava:guava:$guava")
+            dependency("org.openjdk.jmh:jmh-core:$jmh")
+            dependency("org.openjdk.jmh:jmh-generator-annprocess:$jmh")
+            dependency("org.ow2.asm:asm-commons:$asm")
             dependency("org.glassfish:jakarta.json:$glassfishJson")
+            dependency("com.google.errorprone:error_prone_annotations:$errorProneAnnotations")
+            dependency("com.google.j2objc:j2objc-annotations:$j2objcAnnotations")
+            dependency("org.redisson:redisson:$redisson")
+
+            dependency("org.eclipse.jetty.ee10:jetty-ee10-servlet:$jetty")
+            dependency("org.eclipse.jetty:jetty-server:$jetty")
+            dependency("org.eclipse.jetty.ee10:jetty-ee10-webapp:$jetty")
+            dependency("org.eclipse.jetty:jetty-security:$jetty")
+            dependency("org.eclipse.jetty:jetty-http:$jetty")
+            dependency("org.eclipse.jetty:jetty-io:$jetty")
+            dependency("org.eclipse.jetty:jetty-util:$jetty")
+            dependency("org.freemarker:freemarker:$freemarker")
+
+            dependency("org.reflections:reflections:$reflections")
+
+            dependency("org.webjars:sockjs-client:$sockjs")
+            dependency("org.webjars:stomp-websocket:$stomp")
+            dependency("org.webjars:bootstrap:$bootstrap")
+            dependency("org.springdoc:springdoc-openapi-starter-webmvc-ui:$springDocOpenapiUi")
+
+            dependency("com.google.code.findbugs:jsr305:$jsr305")
+
         }
     }
 
