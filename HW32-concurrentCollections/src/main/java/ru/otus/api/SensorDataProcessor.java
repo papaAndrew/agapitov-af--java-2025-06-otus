b4dcec45457e18de44b9bc13d32fbfded2261b44
papaAndrew <a.hermit71@gmail.com>
2025-11-08 19:14:33 +0500

    HW32-concurrentCollections reinit

---
+++ HW32-concurrentCollections/src/main/java/ru/otus/api/SensorDataProcessor.java
@@ -0,0 +1,10 @@
+package ru.otus.api;
+
+import ru.otus.api.model.SensorData;
+
+public interface SensorDataProcessor {
+    void process(SensorData data);
+
+    default void onProcessingEnd() {
+    }
+}
