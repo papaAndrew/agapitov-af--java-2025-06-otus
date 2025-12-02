ee806875ea004f2937c72042ee4e120efd4ada3e
papaAndrew <a.hermit71@gmail.com>
2025-12-02 15:41:17 +0500

    bank-client save

--- ZW-finShop/bank-client/build.gradle.kts
+++ ZW-finShop/bank-client/build.gradle.kts
@@ -1,19 +1,12 @@
-plugins {
-    id("java")
-}
-
-group = "ru.otus"
-version = ""
-
-repositories {
-    mavenCentral()
-}
-
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.springframework.boot:spring-boot-starter-websocket")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("com.google.code.findbugs:jsr305")
+
+    implementation("org.webjars:webjars-locator-core")
+    implementation("org.webjars:sockjs-client")
+    implementation("org.webjars:stomp-websocket")
+    implementation("org.webjars:bootstrap")
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
+
---
+++ ZW-finShop/bank-client/src/main/java/ru/otus/controllers/BankClientController.java
@@ -0,0 +1,111 @@
+package ru.otus.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.event.EventListener;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.support.GenericMessage;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.socket.messaging.SessionSubscribeEvent;
+import org.springframework.web.util.HtmlUtils;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import ru.otus.domain.Message;
+
+@Controller
+public class BankClientController {
+    private static final Logger logger = LoggerFactory.getLogger(BankClientController.class);
+
+    private static final String TOPIC_TEMPLATE = "/topic/response.";
+    private static final String ROOM_1408 = "1408";
+
+    private final WebClient datastoreClient;
+    private final SimpMessagingTemplate template;
+
+    public BankClientController(WebClient datastoreClient, SimpMessagingTemplate template) {
+        this.datastoreClient = datastoreClient;
+        this.template = template;
+    }
+
+    @MessageMapping("/message.{roomId}")
+    public void getMessage(@DestinationVariable("roomId") String roomId, Message message) {
+        logger.info("get message:{}, roomId:{}", message, roomId);
+
+        saveMessage(roomId, message).subscribe(msgId -> logger.info("message send id:{}", msgId));
+
+        var escapedMessage = new Message(HtmlUtils.htmlEscape(message.messageStr()));
+        template.convertAndSend(String.format("%s%s", TOPIC_TEMPLATE, roomId), escapedMessage);
+
+        logger.info("send also roomId:{}", ROOM_1408);
+        template.convertAndSend(String.format("%s%s", TOPIC_TEMPLATE, ROOM_1408), escapedMessage);
+    }
+
+    @EventListener
+    public void handleSessionSubscribeEvent(SessionSubscribeEvent event) {
+        var genericMessage = (GenericMessage<byte[]>) event.getMessage();
+        var simpDestination = (String) genericMessage.getHeaders().get("simpDestination");
+        if (simpDestination == null) {
+            logger.error("Can not get simpDestination header, headers:{}", genericMessage.getHeaders());
+            throw new ChatException("Can not get simpDestination header");
+        }
+        if (!simpDestination.startsWith(TOPIC_TEMPLATE)) {
+            return;
+        }
+        var roomId = parseRoomId(simpDestination);
+
+        var principal = event.getUser();
+        if (principal == null) {
+            return;
+        }
+        logger.info("subscription for:{}, roomId:{}, user:{}", simpDestination, roomId, principal.getName());
+        getMessagesByRoomId(roomId)
+                .doOnError(ex -> logger.error("getting messages for roomId:{} failed", roomId, ex))
+                .subscribe(message -> template.convertAndSendToUser(principal.getName(), simpDestination, message));
+    }
+
+    private long parseRoomId(String simpDestination) {
+        try {
+            var idxRoom = simpDestination.lastIndexOf(TOPIC_TEMPLATE);
+            return Long.parseLong(simpDestination.substring(idxRoom).replace(TOPIC_TEMPLATE, ""));
+        } catch (Exception ex) {
+            logger.error("Can not get roomId", ex);
+            throw new ChatException("Can not get roomId");
+        }
+    }
+
+    private Mono<Long> saveMessage(String roomId, Message message) {
+        checkEnabled(roomId);
+        return datastoreClient
+                .post()
+                .uri(String.format("/msg/%s", roomId))
+                .accept(MediaType.APPLICATION_JSON)
+                .bodyValue(message)
+                .exchangeToMono(response -> response.bodyToMono(Long.class));
+    }
+
+    private Flux<Message> getMessagesByRoomId(long roomId) {
+        return datastoreClient
+                .get()
+                .uri(String.format("/msg/%s", roomId))
+                .accept(MediaType.APPLICATION_NDJSON)
+                .exchangeToFlux(response -> {
+                    if (response.statusCode().equals(HttpStatus.OK)) {
+                        return response.bodyToFlux(Message.class);
+                    } else {
+                        return response.createException().flatMapMany(Mono::error);
+                    }
+                });
+    }
+
+    private void checkEnabled(String roomId) {
+        if (ROOM_1408.equals(roomId.trim())) {
+            logger.error("Room {} is read only", roomId);
+            throw new ChatException("Cannot send messages to this room");
+        }
+    }
+}
