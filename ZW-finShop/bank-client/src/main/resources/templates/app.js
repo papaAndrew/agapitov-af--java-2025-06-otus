e83a3b5e139ac9db41b7e17ed5b7fcd5514ec845
papaAndrew <a.hermit71@gmail.com>
2025-12-26 12:30:25 +0500

    ZW-finShop front

--- ZW-finShop/bank-client/src/main/resources/templates/app.js
+++ ZW-finShop/bank-client/src/main/resources/templates/app.js
@@ -1,12 +1,27 @@
 
 const saveClaim = () => {
+    var btn = document.getElementById('btnSend');
+    if (btn) {
+        if (btn.textContent === 'Новый') {
+            btn.textContent = 'Запросить';
+        } else {
+            btn.textContent = 'Новый';
+            document.getElementById('amount').textContent = '';
+            document.getElementById('period').textContent = '';
+            return;
+        }
+    }
+
  const loanClaim = {
         clientId: document.getElementById('clientId')?.value || '',
         status: document.getElementById('status')?.value || '',
         period: document.getElementById('period')?.value || '',
         amount: document.getElementById('amount')?.value || '',
     };
-
+    if (!loanClaim.period || !loanClaim.amount) {
+        document.getElementById('status').textContent = 'Недостаточно данных';
+        return;
+    }
 
     fetch("http://localhost:8020/claim", {
         method: "POST",
